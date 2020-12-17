6f4f8d5924895c00cb1bc6bc25cad288722a6d1d
tracer-ics2019 <tracer@njuics.org>
2020-12-17 10:31:07 +0800

    >  compile U201714786 root Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:31:07 up  1:03,  1 user,  load average: 0.50, 0.39, 0.43 c4d33e8ce33b88b9e654b8ff53a8b7d710828d4c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -153,23 +153,15 @@ static bool make_token(char *e) {
 }
 
 bool check_parentheses(int p,int q){
-  if(tokens[p].type != TK_LP || tokens[q].type != TK_RP){
-    return false;
-  }
-  int cnt = 0;
-  for (int i = p; i <= q; i++)
-  {
-    if(tokens[i].type == TK_LP){
-      cnt++;
-    }else if(tokens[i].type == TK_RP){
-      cnt--;
-    }
-    assert(cnt >= 0);
-    if(cnt < 0)
-      return false;
-  }
-  assert(cnt == 0);
-  return cnt == 0;
+  if(!((tokens[p].type == TK_LP)&&(tokens[q].type == TK_RP)))return false;
+	int k=0;
+	for(int i=p+1;i<q;i++){
+		if(tokens[i].type == TK_LP)k++;
+		if(tokens[i].type == TK_RP)k--;
+		if(k<0)return false;
+	}
+	if(k == 0)return true;
+	else return false;
 }
 
 int find_mainop(int p,int q){
