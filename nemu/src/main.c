9028d22cac19fcf459df405e5eec6bb589e95795
tracer-ics2019 <tracer@njuics.org>
2020-12-17 10:01:15 +0800

    >  compile U201714786 root Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:01:15 up 33 min,  1 user,  load average: 1.00, 0.54, 0.47 c97f3f7a53cc14c8f04cf5f642186d16c7d8b4c3

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -12,7 +12,34 @@ int main(int argc, char *argv[]) {
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
-  FILE *fp = fopen("/home/ics2019/nemu/tools/gen-expr/input", "r");
+	bool success = true;
+
+	FILE *fp = fopen("/home/mff/ics2019/nemu/tools/gen-expr/input", "r");
+	assert(fp != NULL);
+	unsigned int result = 0;
+	char inputexprbuf[65536];
+	size_t len = 0;
+	ssize_t read;
+	char *line = NULL;
+	int fileline = 1;
+
+	while((read = getline(&line, &len, fp)) != EOF){
+		assert(strlen(line) < 65536);
+		strcat(inputexprbuf, line);
+		char x[20];
+		sscanf(inputexprbuf, "%s", x);
+		int x_len = strlen(x);
+		sscanf(inputexprbuf, "%u", &result);
+		if(result == expr(line + x_len+1, &success)){
+			printf("success\n");
+			memset(inputexprbuf, 0, sizeof(inputexprbuf));
+		}
+		else{
+			printf("error occurs in %d\n", fileline);
+			break;
+		}
+		fileline++;
+	}
 	fclose(fp);
 
   return 0;
