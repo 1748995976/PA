a46e864b98f4bd5a8f1d798bbb1c69841d9646dd
tracer-ics2019 <tracer@njuics.org>
2020-12-17 10:17:11 +0800

    >  compile U201714786 root Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:17:11 up 49 min,  1 user,  load average: 0.62, 0.48, 0.45 df157bd0e017c7008f9d784be5695a472cd1d6d6

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -8,7 +8,6 @@ void ui_mainloop(int);
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
-  
   FILE *fp = fopen("/home/hust/ics2019/nemu/tools/gen-expr/input","r");
   char str[100];
   uint32_t result;
@@ -19,13 +18,12 @@ int main(int argc, char *argv[]) {
 	  printf("%s\n",str);
 	  //uint32_t count_result = expr("2+3*4",&success);
 	  uint32_t count_result =  expr(str,&success);
-	  printf("%u %s %u\n",result,str,count_result);
+	  printf("--------------%u %s %u\n",result,str,count_result);
 	  //assert(count_result == result);
-	  if(count_result == result)printf("yes\n");
-	  else printf("no\n");
+	  //if(count_result == result)printf("yes\n");
+	  //else printf("no\n");
 	  }
 	  fclose(fp);
-
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
   return 0;
