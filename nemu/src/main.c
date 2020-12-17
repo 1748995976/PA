c113dfb8e98f565271d7543a2232f7038e39c03c
tracer-ics2019 <tracer@njuics.org>
2020-12-17 10:21:22 +0800

    >  compile U201714786 root Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:21:22 up 53 min,  1 user,  load average: 0.88, 0.56, 0.48 5adc1d86db5fb921ee84b776dffdf84e917fb46d

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -2,6 +2,7 @@
 #include "nemu.h"
 #include <stddef.h>
 #include <errno.h>
+#include <stdlib.h>
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
@@ -9,21 +10,22 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
   FILE *fp = fopen("/home/hust/ics2019/nemu/tools/gen-expr/input","r");
-  char str[100];
+  char str[1000];
   uint32_t result;
   while(!feof(fp)){
 	  fscanf(fp,"%u",&result);
-	  fscanf(fp,"%s",str);
+	  fscanf(fp,"%[^\n]",str);
 	  bool success ;
 	  printf("%s\n",str);
-	  //uint32_t count_result = expr("2+3*4",&success);
 	  uint32_t count_result =  expr(str,&success);
 	  printf("--------------%u %s %u\n",result,str,count_result);
 	  //assert(count_result == result);
-	  //if(count_result == result)printf("yes\n");
-	  //else printf("no\n");
-	  }
-	  fclose(fp);
+	  if(count_result == result)
+      printf("yes\n");
+	  else 
+      printf("no\n");
+	}
+	fclose(fp);
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
   return 0;
