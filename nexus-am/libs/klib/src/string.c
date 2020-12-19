b81d0643a0550d72951b2a1104e7d09dd7d76f98
U201714786WangZhancheng <1748995976@qq.com>
2020-12-19 18:59:08 +0800

    finish string.c

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,39 +3,117 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  int cnt;
+	for(cnt = 0; s[cnt] != '\0'; cnt++);
+	return cnt;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  int cnt;
+	for(cnt = 0; src[cnt] != '\0'; cnt++){
+		dst[cnt] = src[cnt];
+	}
+	dst[cnt] = '\0';
+	return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  int cnt;
+	for(cnt = 0; cnt < n && src[cnt] != '\0'; cnt++){
+		dst[cnt] = src[cnt];
+	}
+	while(cnt < n){
+		dst[cnt] = '\0';
+	}
+	return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  int n = strlen(dst);
+	int cnt;
+	for(cnt = 0; src[cnt] != '\0'; cnt++){
+		dst[n+cnt] = src[cnt];
+	}
+	dst[n+cnt] = '\0';
+	return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  int n1 = strlen(s1);
+	int n2 = strlen(s2);
+	int cnt;
+	for(cnt = 0; cnt < n1 && cnt < n2; cnt++){
+		if(s1[cnt] < s2[cnt]){
+			return -1;
+		}
+		else if(s1[cnt] > s2[cnt]){
+			return 1;
+		}
+	}
+	if(cnt == n1 && cnt == n2){
+		return 0;
+	}
+	else if(cnt < n1){
+		return 1;
+	}else{
+    return -1;
+  }
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  int n1 = strlen(s1);
+	int n2 = strlen(s2);
+	int cnt;
+	for(cnt = 0; cnt < n1 && cnt < n2 && cnt < n; cnt++){
+		if(s1[cnt] < s2[cnt]){
+			return -1;
+		}
+		else if(s1[cnt] > s2[cnt]){
+			return 1;
+		}
+	}
+	if(cnt == n || (cnt == n1 && cnt == n2)){
+		return 0;
+	}
+	else if(cnt < n1){
+		return 1;
+	}else{
+    return -1;
+  }
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  unsigned char ch = (unsigned char)c;
+	unsigned char *p = (unsigned char *)v;
+	int cnt;
+	for(cnt = 0; cnt < n; cnt++){
+		p[cnt] = ch;
+	}
+	return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  char *a = (char *)out;
+	char *b = (char *)in;
+	int cnt;
+	for(cnt = 0; cnt < n; cnt ++){
+		a[cnt] = b[cnt];
+	}
+	return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+  int cnt;
+	unsigned char *p = (unsigned char *)s1, *q = (unsigned char *)s2;
+	for(cnt = 0; cnt < n; cnt++){
+		if(p[cnt] < q[cnt]){
+			return -1;
+		}
+		else if(p[cnt] > q[cnt]){
+			return 1;
+		}
+	}
+	return 0;
 }
 
 #endif
