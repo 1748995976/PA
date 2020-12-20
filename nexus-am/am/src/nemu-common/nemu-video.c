2f89c5518c7180ddcb64b80dbd3dac6ef8f08117
U201714786WangZhancheng <1748995976@qq.com>
2020-12-20 18:51:20 +0800

    finish  VGA

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -1,6 +1,9 @@
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
+static inline int min(int x, int y) {
+  return (x < y) ? x : y;
+}
 
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
@@ -19,6 +22,19 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+      int width = screen_width();
+      int height = screen_height();
+      int cp_bytes = sizeof(uint32_t) * ((w < (width-x))? w : (width-x));
+
+      uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+
+      for (int i = 0; i < h && y + i < height; i++){
+        memcpy(&fb[(y + i) * width + x], pixels, cp_bytes);
+        pixels += w;
+      }
 
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
