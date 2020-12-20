93286fc085999f7b22707df08293aacb628ae86c
U201714786WangZhancheng <1748995976@qq.com>
2020-12-20 16:53:47 +0800

    finish diff test and pa2.3 without VGA

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -2,7 +2,18 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  int i;
+	for(i = 0; i < 32; i++){
+		if(ref_r->gpr[i]._32 != cpu.gpr[i]._32){
+      printf("%d reg is diff!\n",i);
+			return false;
+		}
+	}
+	// if(ref_r->pc != pc){
+  //   printf("pc is diff!\n");
+	// 	return false;
+	// }
+	return true;
 }
 
 void isa_difftest_attach(void) {
