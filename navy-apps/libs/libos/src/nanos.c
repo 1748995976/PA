49f4e5478a7238e29d4abca18a3de406ba6362a0
U201714786WangZhancheng <1748995976@qq.com>
2020-12-23 14:26:08 +0800

    no add

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -56,13 +56,12 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  _exit(SYS_open);
-  return 0;
+  int ret = _syscall_(SYS_open, (intptr_t)path, flags, mode);
+  return ret;
 }
 
 int _write(int fd, void *buf, size_t count) {
   int ret = _syscall_(SYS_write, fd, (intptr_t)buf, count);
-  //_exit(SYS_write);
   return ret;
 }
 
@@ -85,18 +84,18 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  _exit(SYS_read);
-  return 0;
+  int ret = _syscall_(SYS_read, fd, (intptr_t)buf, count);
+  return ret;
 }
 
 int _close(int fd) {
-  _exit(SYS_close);
-  return 0;
+  int ret = _syscall_(SYS_close, fd, 0, 0);
+  return ret;
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _exit(SYS_lseek);
-  return 0;
+  off_t ret = _syscall_(SYS_lseek, fd, (intptr_t)offset, whence);
+  return ret;
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
