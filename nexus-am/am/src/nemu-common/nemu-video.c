7cb671b8d5682d9fff3e63f7b518b34ac4520567
U201714786WangZhancheng <1748995976@qq.com>
2020-12-25 17:43:20 +0800

    store one stage

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -5,6 +5,10 @@ static inline int min(int x, int y) {
   return (x < y) ? x : y;
 }
 
+int screen_width();
+
+int screen_height();
+
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
@@ -46,9 +50,9 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_vga_init() {
-  int i;
-  int size = screen_width() * screen_height();
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < size; i ++) fb[i] = i;
-  draw_sync();
+  // int i;
+  // int size = screen_width() * screen_height();
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < size; i ++) fb[i] = i;
+  // draw_sync();
 }
